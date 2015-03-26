95c1bfdb5aafd225f614b3d9e84aac69e8d82af8
shengyu.guo <guoshengyu2013@gmail.com>
2015-03-26 22:12:16 +0800

    Put full information into Selection data format

--- src/main/java/caup/dataloader/controller/MainController.java
+++ src/main/java/caup/dataloader/controller/MainController.java
@@ -4,6 +4,7 @@ import caup.dataloader.entity.DimIndicator3Entity;
 import caup.dataloader.service.SelectionService;
 import caup.dataloader.transformer.reader.CoreIndexSearcher;
 import caup.dataloader.transformer.reader.DataModel.InputDataFormat;
+import caup.dataloader.transformer.reader.DataModel.SelectionDataFormat;
 import caup.dataloader.transformer.reader.DataReader;
 import caup.dataloader.transformer.reader.LuceneTest;
 import caup.dataloader.util.StringUtils;
@@ -36,30 +37,62 @@ public class MainController {
     public String mainPageDisplay(ModelMap model) {
         model.addAttribute("msg", "Main Page");
 
+getLuceneSeachResult();
 
+        return "MainPage";
+    }
+
+    public List<SelectionDataFormat> getLuceneSeachResult(){
+        List<SelectionDataFormat> ret = new ArrayList<SelectionDataFormat>();
+        SelectionDataFormat selectionDataFormat = null;
         List<DimIndicator3Entity> dimIndicator3EntityList = selectionService.getIndicator().getData();
         try {
             DataReader reader = new DataReader("D:\\IntelliJWorkspace\\DataLoader\\src\\main\\webapp\\datafile\\Jiaxing-shiqu-Test.xlsx", 1, 100, 2, true);
             List<InputDataFormat> inputDataFormatList = reader.getYearBookIndexList();
 
+            //Put the preprocessed data and the orginal data into map
             Map<String, String> orgIndexMap = new HashMap<String, String>();
             for (InputDataFormat inputDataFormat : inputDataFormatList) {
                 orgIndexMap.put(StringUtils.yearbookIndexPreprocess(inputDataFormat.getIndexName().replaceAll(" ", "")), inputDataFormat.getIndexName().replaceAll(" ", ""));
             }
-
-            for(int i = 0; i != 30; i++) {
-
-                CoreIndexSearcher coreIndexSearcher = new CoreIndexSearcher(dimIndicator3EntityList.get(i).getIndexName(), new ArrayList<String>(orgIndexMap.keySet()));
-                List<String> result = coreIndexSearcher.getTopYearbookIndex();
-                System.out.println(dimIndicator3EntityList.get(i).getIndexName());
-                for(String str: result)
-                    System.out.print(orgIndexMap.get(str) + " | ");
-                System.out.println();
-                System.out.println("------");
-            }
+            searchForAllIndex(ret, dimIndicator3EntityList, inputDataFormatList, orgIndexMap);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "MainPage";
+
+        return ret;
+    }
+
+    private void searchForAllIndex(List<SelectionDataFormat> ret, List<DimIndicator3Entity> dimIndicator3EntityList, List<InputDataFormat> inputDataFormatList, Map<String, String> orgIndexMap) throws Exception {
+        SelectionDataFormat selectionDataFormat;
+        for(int i = 0; i != 30; i++) {
+
+            CoreIndexSearcher coreIndexSearcher = new CoreIndexSearcher(dimIndicator3EntityList.get(i).getIndexName(), new ArrayList<String>(orgIndexMap.keySet()));
+            List<String> result = coreIndexSearcher.getTopYearbookIndex();
+           // System.out.println(dimIndicator3EntityList.get(i).getIndexName());
+            Map<String, String> map =new HashMap<String, String>();
+            for(String str: result){
+
+                map.put(orgIndexMap.get(str), searchFromInputDataModel(inputDataFormatList,orgIndexMap.get(str)).getUnit());
+            }
+            selectionDataFormat = new SelectionDataFormat();
+            selectionDataFormat.setDatabaseIndex(dimIndicator3EntityList.get(i).getIndexName());
+            selectionDataFormat.setDatabaseUnit(dimIndicator3EntityList.get(i).getUnit());
+            selectionDataFormat.setYearbookIndexUnitMap(map);
+
+            for(String str: result)
+                System.out.print(orgIndexMap.get(str) + " | ");
+            System.out.println();
+            System.out.println("------");
+            ret.add(selectionDataFormat);
+        }
+    }
+
+    private InputDataFormat searchFromInputDataModel(List<InputDataFormat> inputDataFormatList, String yearbookIndex){
+        for(InputDataFormat item: inputDataFormatList){
+            if(item.getIndexName().equals(yearbookIndex))
+                return item;
+        }
+        return new InputDataFormat();
     }
 }
--- src/main/java/caup/dataloader/transformer/reader/DataModel/SelectionDataFormat.java
+++ src/main/java/caup/dataloader/transformer/reader/DataModel/SelectionDataFormat.java
@@ -1,6 +1,7 @@
 package caup.dataloader.transformer.reader.DataModel;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Richard on 2015/03/23 .
@@ -9,6 +10,40 @@ public class SelectionDataFormat {
     String region;
     String databaseIndex;
     String databaseUnit;
-    List<String> yearbookIndexList;
-    String yearbookUnit;
+    Map<String, String> yearbookIndexUnitMap;
+
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDatabaseIndex() {
+        return databaseIndex;
+    }
+
+    public void setDatabaseIndex(String databaseIndex) {
+        this.databaseIndex = databaseIndex;
+    }
+
+    public String getDatabaseUnit() {
+        return databaseUnit;
+    }
+
+    public void setDatabaseUnit(String databaseUnit) {
+        this.databaseUnit = databaseUnit;
+    }
+
+    public Map<String, String> getYearbookIndexUnitMap() {
+        return yearbookIndexUnitMap;
+    }
+
+    public void setYearbookIndexUnitMap(Map<String, String> yearbookIndexList) {
+        this.yearbookIndexUnitMap = yearbookIndexList;
+    }
+
+
 }
